7ffd309b4b95335943812d9af551955cc49fd375
Shivam Bhatt <94529313+ShivamB15@users.noreply.github.com>
2025-05-12 00:18:10 +0530

    Update PredictForm.js

--- src/components/PredictForm.js
+++ src/components/PredictForm.js
@@ -11,7 +11,7 @@ export default function PredictForm() {
   const handlePredict = async () => {
     setLoading(true);
     try {
-      const response = await fetch('http://127.0.0.1:3003/predict', {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text: inputText }),
